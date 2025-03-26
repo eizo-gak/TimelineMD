c031d339cf0d1e92082c98c47228b49e436cee70
eizo-gak <setsugen2019@gmail.com>
2025-03-26 22:53:12 +0900

    fix server.py

--- app/server.py
+++ app/server.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Server is Online."}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
